0c1e61f28f009f0d3d39a39777ab03766653b8d8
SniperBerserker <firewire617@gmail.com>
2022-07-21 16:42:02 +0200

    dynamische pagina verder afgemmakt

--- examenproject/script/dyn_data.js
+++ examenproject/script/dyn_data.js
@@ -22,17 +22,17 @@ const filter_data = [
 ]
 
 let producten_data = [
-    ["Membrane keyboards","Membrane keyboard zwart met RBG lichten", "column card RBG fullSize normaal", "../media/membraneKeyboard/RBGKeyboardZwart.jpg", "productendetails/productdetailpagina.html", "Membrane keyboard zwart met RBG lichten", "19,99 euro"],
+    ["Membrane keyboards", "Membrane keyboard zwart met RBG lichten", "column card RBG fullSize normaal", "../media/membraneKeyboard/RBGKeyboardZwart.jpg", "productendetails/productdetailpagina.html", "Membrane keyboard zwart met RBG lichten", "19,99 euro"],
     ["Membrane keyboards", "Membrane keyboard wit met RBG lichten", "column card RBG fullSize normaal", "../media/membraneKeyboard/RBGKeyboardWit.jpg", "productendetails/RBGKeyboardWit.html", "Membrane keyboard wit met RBG lichten", "24,99euro"],
     ["Membrane keyboards", "Compact zwart Keyboard", "column card compact geenLichten normaal", "../media/membraneKeyboard/CompactZwartKeyboard.jpg", "productendetails/CompactZwartKeyboard.html", "Compact zwart Keyboard", "10 euro"],
     ["Membrane keyboards", "Ergonomic zwart keyboard", "column card ergonomic geenLichten fullSize", "../media/membraneKeyboard/ErgonomicZwartKeyboard.jpg", "productendetails/ErgonomicZwartKeyboard.html", "Ergonomic zwart keyboard", "19,99 euro"],
     ["Membrane keyboards", "Type machine keyboard", "column card fullSize geenLichten normaal", "../media/membraneKeyboard/TypeMachineStijlWitKeyboard.jpg", "productendetails/TypeMachineStijlWitKeyboard.html", "Type machine stijl wit keyboard", "30 euro"],
-    ["Membrane keyboards", "Keyboard Met Touchpad", "../media/membraneKeyboard/KeyboardMetTouchpad.jpg", "../media/membraneKeyboard/KeyboardMetTouchpad.jpg", "", "Keyboard Met Touchpad", "40 euro"],
+    ["Membrane keyboards", "Keyboard Met Touchpad", "column card normaal fullSize geenLichten", "../media/membraneKeyboard/KeyboardMetTouchpad.jpg", "../media/membraneKeyboard/KeyboardMetTouchpad.jpg", "", "Keyboard Met Touchpad", "40 euro"],
     ["Membrane keyboards", "Opvouwbaar keyboard", "column card opvouwbaar fullSize geenLichten", "../media/membraneKeyboard/OpvouwbaarKeyboard.jpg", "", "Opvouwbaar Keyboard", "35 euro"],
     ["Membrane keyboards", "Silicone Keyboard", "column card opvouwbaar fullSize geenLichten", "../media/membraneKeyboard/SiliconeKeyboard.jpg", "", "Silicone Keyboard", "4,99euro"],
 
     ["Mechanical keyboards", "Compact mechanical keyboard met blauwe lichten", "column card compact blauwe normaal", "../media/mechanicalKeyboard/CompactKeyboardBlauwEnZwart.jpg", "", "Compact mechanical keyboard zwart met blauwe lichten", "20 euro"],
-    ["Mechanical keyboards", "Ergonomic keyboard groen", "column card fullSize groene ergonomic", "../media/mechanicalKeyboard/ErgonomicKeyboardGroen.png","", "Ergonomic keyboard groen", "Ergonomic keyboard groen", "13 euro"],
+    ["Mechanical keyboards", "Ergonomic keyboard groen", "column card fullSize groene ergonomic", "../media/mechanicalKeyboard/ErgonomicKeyboardGroen.png", "", "Ergonomic keyboard groen", "13 euro"],
     ["Mechanical keyboards", "Draadloze Wit Keyboard", "column card fullSize blauwe normaal", "../media/mechanicalKeyboard/DraadlozeWitKeyboard.jpg", "", "Draadloze Wit Keyboard", "21,99 euro"],
     ["Mechanical keyboards", "Logitech G Wit Keyboard", "column card fullSize RBG normaal", "../media/mechanicalKeyboard/LogitechGWiKeyboard.png", "", "Logitech G Wit Keyboard", "33 euro"],
     ["Mechanical keyboards", "Metaal Zwart Keyboard met lichten", "column card fullSize rode normaal", "../media/mechanicalKeyboard/MetaalZwartKeyboard.jpg", "", "Metaal Zwart Keyboard met lichten", "50 euro"],
--- examenproject/script/dyn_generate.js
+++ examenproject/script/dyn_generate.js
@@ -3,18 +3,19 @@ window.addEventListener("load", init)
 let producten = [];
 class Product {
 
-    constructor(naam, prijs , image , alt) {
+    constructor(naam, prijs , image , alt , C) {
         this.naam = naam;
         this.prijs = prijs;
         this.image = image;
         this.alt = alt;
-
+        this.C = C;
     }
 
     maakHtml() {
-        return `<h4>${this.naam}</h4>` + `<p>${this.prijs}</p>` + `<img src=${this.image}  alt=${this.alt}/>` ;
+        return  `<section class="${this.C}" >  <img src=${this.image}  alt="${this.alt}"> <h4>${this.naam}</h4>  <p>${this.prijs}</p> </section> `  ;
 
     }
+
 
 }
 class Categorie {
@@ -28,7 +29,7 @@ class Categorie {
     }
 
     maakHtml() {
-        let html = `<h3>${this.naam}</h3>`;
+        let html = `<section class="row">` + `<h3>${this.naam}</h3>`;
         for (let prob of this.producten) {
             html += prob.maakHtml();
         }
@@ -36,11 +37,67 @@ class Categorie {
     }
 }
 
+
 function showCategorieen() {
     let body = document.querySelector('body');
+    body.setAttribute("class", "grid")
+
+    let main = document.createElement("main")
+    main.setAttribute("class", "panel grid-main");
+
+    let producten = document.createElement("section");
+    producten.setAttribute("class", "producten");
+    let h2 =document.createElement("h2")
+    h2.setAttribute("class","verborgen")
+    let h2titel = document.createTextNode("Overzicht")
+
+    // voor de filter te maken
+    let filter = document.createElement("section");
+    filter.setAttribute("id", "filter");
+    let h3 =document.createElement("h3")
+    let h3titel = document.createTextNode("filter")
+
+    let Type = document.createElement("section");
+    let h4Type =document.createElement("h4")
+    h4Type.setAttribute("class", "filterType");
+    let typeTitel = document.createTextNode("type")
+
+    let Grootte = document.createElement("section");
+    let h4grootte =document.createElement("h4")
+    h4grootte.setAttribute("class", "filtergrootte");
+    let grootteTitel = document.createTextNode("grootte")
+
+    let Lichten = document.createElement("section");
+    let h4Lichten =document.createElement("h4")
+    h4Lichten.setAttribute("class", "filterLichten");
+    let lichtenTitel = document.createTextNode("Lichten")
+
+    body.appendChild(main);
+    main.appendChild(producten);
+    producten.appendChild(h2)
+    h2.appendChild(h2titel)
+
+    producten.appendChild(filter)
+    filter.appendChild(h3)
+    h3.appendChild(h3titel)
+
+    filter.appendChild(Type)
+    Type.appendChild(h4Type)
+    h4Type.appendChild(typeTitel)
+
+    filter.appendChild(Grootte)
+    Grootte.appendChild(h4grootte)
+    h4grootte.appendChild(grootteTitel)
+
+    filter.appendChild(Lichten)
+    Lichten.appendChild(h4Lichten)
+    h4Lichten.appendChild(lichtenTitel)
+
+
     for (let categorie of categorieen) {
-        body.innerHTML += categorie.maakHtml();
+        producten.innerHTML +=  categorie.maakHtml();
     }
+
 }
 
 let categorieen = [];
@@ -48,7 +105,7 @@ let categorieen = [];
 function init() {
     let categorie = "";
     for (let i = 0; i < producten_data.length; i++) {
-        let product = new Product(producten_data[i][1], producten_data[i][6],producten_data[i][3],producten_data[i][5]);
+        let product = new Product(producten_data[i][1], producten_data[i][6],producten_data[i][3],producten_data[i][5],producten_data[i][2]);
         if (categorie !== producten_data[i][0]) {
             categorie = producten_data[i][0];
             categorieen.push(new Categorie(categorie));
@@ -58,7 +115,7 @@ function init() {
 
     console.log(categorieen);
     showCategorieen();
-}
 
+}
 
 
